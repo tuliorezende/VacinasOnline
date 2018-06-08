c80cbe15a72616eadd40f63cdf7b55516e0ca951
tuliorezende <tulio.rezende@outlook.com>
2018-06-08 17:31:20 -0300

    create main menu

--- app/src/main/java/com/example/tulior/vacinasonline/AccountSuccessActivity.java
+++ app/src/main/java/com/example/tulior/vacinasonline/AccountSuccessActivity.java
@@ -1,7 +1,9 @@
 package com.example.tulior.vacinasonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AccountSuccessActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class AccountSuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_success);
+    }
+
+    public void sendToMainMenu(View v) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+
     }
 }
